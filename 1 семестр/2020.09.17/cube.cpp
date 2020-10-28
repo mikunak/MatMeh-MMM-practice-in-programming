a01303c3170509a40f6b06585f70650ef4df5bf8
mikunak <71778735+mikunak@users.noreply.github.com>
2020-10-28 22:37:46 +0300

    Add files via upload

---
+++ 1 семестр/2020.09.17/cube.cpp
@@ -0,0 +1,23 @@
+//Написать программу находящую среднее арифметическое кубов последовательности чисел
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+float function(int n){
+    int S;
+    S = 0;
+    for(int i = 1; i <= n; i++){
+        int x;
+        cin >> x;
+        S += x*x*x;
+    }
+    return (double)S/n;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << function(n);
+    return 0;
+}
---
+++ 1 семестр/2020.09.24/count_sort.cpp
@@ -0,0 +1,20 @@
+//Написать функцию сортировки массива, использующую указательную арифметику вместо обращения по индексам. 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int *A = new int[n];
+    int *cnt = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+    for (int i = 0; i < n; i++)
+        cnt[A[i] - 1] += 1;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < cnt[i]; j++)
+            cout << i + 1;
+    return 0;
+}
